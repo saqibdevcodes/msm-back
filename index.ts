b00ce3fcae60e280b097719f1fd513e2347dbb92
saqibdevcodes <saqibiris.co@gmail.com>
2026-09-14 16:29:29 +0500

    refactor: update CORS configuration to support multiple origins

--- index.ts
+++ index.ts
@@ -5,7 +5,10 @@ const app = express();
 import cors from "cors";
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "https://msm.iriscommunications.cloud",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
